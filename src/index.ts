57514d1c82bebe2eefc98d92f3f2d5c393c8c67a
PMK744 <69542280+PMK744@users.noreply.github.com>
2023-10-11 12:42:40 -0500

    push

--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@ class BinaryStream {
    */
   public constructor(buffer?: Buffer, offset?: number) {
     this.binary = buffer ? [...buffer] : []
-    this.readOffset = offset || 0
-    this.writeOffset = offset || 0
+    this.readOffset = offset ?? 0
+    this.writeOffset = offset ?? 0
   }
 
   /**
@@ -71,6 +71,14 @@ class BinaryStream {
   }
 
   /**
+   * Skips a number of bytes
+   * @param length 
+   */
+  public skip(length: number): void {
+    this.readOffset += length
+  }
+
+  /**
    * Reads from the stream
    * @param length 
    * @returns {Buffer}
@@ -103,7 +111,7 @@ class BinaryStream {
    * Writes a unsigned byte ( 0 to 255 )
    * @param value 
    */
-  public writeByte(value: number): void {
+  public writeByte(value: number, unsafe = false): void {
     this.binary[this.writeOffset++] = value
   }
 
@@ -112,7 +120,11 @@ class BinaryStream {
    * @returns {number}
    */
   public readInt8(): number {
-    return this.readByte()
+    const value = this.binary[this.readOffset++] || 0
+
+    if (value < -128 || value > 127) return null
+
+    return value
   }
 
   /**
@@ -120,7 +132,8 @@ class BinaryStream {
    * @param value 
    */
   public writeInt8(value: number): void {
-    this.writeByte(value)
+    if (value < -128 || value > 127) throw Error('Value must be between -128 and 127')
+    this.binary[this.writeOffset++] = value
   }
 
   /**
@@ -128,7 +141,11 @@ class BinaryStream {
    * @returns {number}
    */
   public readUInt8(): number {
-    return this.readByte()
+    const value = this.binary[this.readOffset++] || 0
+
+    if (value < 0 || value > 255) return null
+
+    return value
   }
 
   /**
@@ -136,7 +153,8 @@ class BinaryStream {
    * @param value 
    */
   public writeUInt8(value: number): void {
-    this.writeByte(value)
+    if (value < 0 || value > 255) throw Error('Value must be between 0 and 255')
+    this.binary[this.writeOffset++] = value
   }
 
   /**
@@ -144,8 +162,12 @@ class BinaryStream {
    * @returns {number}
    */
   public readInt16(): number {
-    return this.readByte() << 8 |
+    const value = this.readByte() << 8 |
     this.readByte()
+
+    if (value < -32768 || value > 32767) return null
+
+    return value
   }
 
   /**
@@ -153,6 +175,7 @@ class BinaryStream {
    * @param value 
    */
   public writeInt16(value: number): void {
+    if (value < -32768 || value > 32767) throw Error('Value must be between -32768 and 32767')
     this.writeByte(value >> 8)
     this.writeByte(value & 0xFF)
   }
@@ -162,8 +185,12 @@ class BinaryStream {
    * @returns {number}
    */
   public readUInt16(): number {
-    return this.readByte() << 8 |
+    const value = this.readByte() << 8 |
     this.readByte()
+
+    if (value < 0 || value > 65535) return null
+
+    return value
   }
 
   /**
@@ -171,6 +198,7 @@ class BinaryStream {
    * @param value 
    */
   public writeUInt16(value: number): void {
+    if (value < 0 || value > 65535) throw Error('Value must be between 0 and 65535')
     this.writeByte(value >> 8)
     this.writeByte(value & 0xFF)
   }
@@ -180,10 +208,14 @@ class BinaryStream {
    * @returns {number}
    */
   public readInt32(): number {
-    return this.readByte() << 24 |
+    const value = this.readByte() << 24 |
     this.readByte() << 16 |
     this.readByte() << 8 |
     this.readByte()
+
+    if (value < -2147483648 || value > 2147483647) return null
+
+    return value
   }
 
   /**
@@ -191,6 +223,7 @@ class BinaryStream {
    * @param value 
    */
   public writeInt32(value: number): void {
+    if (value < -2147483648 || value > 2147483647) throw Error('Value must be between -2147483648 and 2147483647')
     this.writeByte(value >> 24)
     this.writeByte(value >> 16)
     this.writeByte(value >> 8)
@@ -202,10 +235,14 @@ class BinaryStream {
    * @returns {number}
    */
   public readUInt32(): number {
-    return this.readByte() << 24 |
+    const value = this.readByte() << 24 |
     this.readByte() << 16 |
     this.readByte() << 8 |
     this.readByte()
+
+    if (value < 0 || value > 4294967295) return null
+
+    return value
   }
 
   /**
@@ -213,6 +250,7 @@ class BinaryStream {
    * @param value 
    */
   public writeUInt32(value: number): void {
+    if (value < 0 || value > 4294967295) throw Error('Value must be between 0 and 4294967295')
     this.writeByte(value >> 24)
     this.writeByte(value >> 16)
     this.writeByte(value >> 8)
@@ -224,7 +262,12 @@ class BinaryStream {
    * @returns {number}
    */
   public readShort(): number {
-    return this.readByte() << 8 | this.readByte()
+    const value = this.readByte() << 8 |
+    this.readByte()
+
+    if (value < -32768 || value > 32767) return null
+
+    return value
   }
 
   /**
@@ -232,6 +275,7 @@ class BinaryStream {
    * @param value 
    */
   public writeShort(value: number): void {
+    if (value < -32768 || value > 32767) throw Error('Value must be between -32768 and 32767')
     this.writeByte(value >> 8)
     this.writeByte(value & 0xFF)
   }
@@ -241,7 +285,12 @@ class BinaryStream {
    * @returns {number}
    */
   public readUShort(): number {
-    return this.readByte() << 8 | this.readByte()
+    const value = this.readByte() << 8 |
+    this.readByte()
+
+    if (value < 0 || value > 65535) return null
+
+    return value
   }
 
   /**
@@ -249,6 +298,7 @@ class BinaryStream {
    * @param value 
    */
   public writeUShort(value: number): void {
+    if (value < 0 || value > 65535) throw Error('Value must be between 0 and 65535')
     this.writeByte(value >> 8)
     this.writeByte(value & 0xFF)
   }
@@ -258,7 +308,7 @@ class BinaryStream {
    * @returns {bigint}
    */
   public readLong(): bigint {
-    return BigInt(this.readByte()) << 56n |
+    const value = BigInt(this.readByte()) << 56n |
     BigInt(this.readByte()) << 48n |
     BigInt(this.readByte()) << 40n |
     BigInt(this.readByte()) << 32n |
@@ -266,6 +316,11 @@ class BinaryStream {
     BigInt(this.readByte()) << 16n |
     BigInt(this.readByte()) << 8n |
     BigInt(this.readByte())
+
+    // TODO: figure out how to check if value is between -9223372036854775808 and 9223372036854775807
+    // if (value < -9223372036854775808n || value > 9223372036854775807n) return null
+
+    return value
   }
 
   /**
@@ -273,6 +328,7 @@ class BinaryStream {
    * @param value 
    */
   public writeLong(value: bigint): void {
+    if (value < -9223372036854775808n || value > 9223372036854775807n) throw Error('Value must be between -9223372036854775808 and 9223372036854775807')
     this.writeByte(Number(value >> 56n))
     this.writeByte(Number(value >> 48n))
     this.writeByte(Number(value >> 40n))
@@ -316,6 +372,106 @@ class BinaryStream {
   public writeString(value: string): void {
     this.writeUInt16(value.length)
     this.write(Buffer.from(value))
+  }
+
+  /**
+   * Reads a 24 bit ( 3 bytes ) signed big eddian integer ( -8388608 to 8388607 )
+   * @returns {number}
+   */
+  public readInt24BE(): number {
+    const value = this.readByte() << 16 |
+    this.readByte() << 8 |
+    this.readByte()
+
+    if (value < -8388608 || value > 8388607) return null
+
+    return value
+  }
+
+  /**
+   * Writes a 24 bit ( 3 bytes ) signed big eddian integer ( -8388608 to 8388607 )
+   * @param value 
+   */
+  public writeInt24BE(value: number): void {
+    if (value < -8388608 || value > 8388607) throw Error('Value must be between -8388608 and 8388607')
+    this.writeByte(value >> 16)
+    this.writeByte(value >> 8)
+    this.writeByte(value)
+  }
+
+  /**
+   * Reads a 24 bit ( 3 bytes ) unsigned big eddian integer ( 0 to 16777215 )
+   * @returns {number}
+   */
+  public readUInt24BE(): number {
+    const value = this.readByte() << 16 |
+    this.readByte() << 8 |
+    this.readByte()
+
+    if (value < 0 || value > 16777215) return null
+
+    return value
+  }
+
+  /**
+   * Writes a 24 bit ( 3 bytes ) unsigned big eddian integer ( 0 to 16777215 )
+   * @param value 
+   */
+  public writeUInt24BE(value: number): void {
+    if (value < 0 || value > 16777215) throw Error('Value must be between 0 and 16777215')
+    this.writeByte(value >> 16)
+    this.writeByte(value >> 8)
+    this.writeByte(value)
+  }
+
+  /**
+   * Reads a 24 bit ( 3 bytes ) signed little eddian integer ( -8388608 to 8388607 )
+   * @returns {number}
+   */
+  public readInt24LE(): number {
+    const value = this.readByte() |
+    this.readByte() << 8 |
+    this.readByte() << 16
+
+    if (value < -8388608 || value > 8388607) return null
+
+    return value
+  }
+
+  /**
+   * Writes a 24 bit ( 3 bytes ) signed little eddian integer ( -8388608 to 8388607 )
+   * @param value 
+   */
+  public writeInt24LE(value: number): void {
+    if (value < -8388608 || value > 8388607) throw Error('Value must be between -8388608 and 8388607')
+    this.writeByte(value)
+    this.writeByte(value >> 8)
+    this.writeByte(value >> 16)
+  }
+
+  /**
+   * Reads a 24 bit ( 3 bytes ) unsigned little eddian integer ( 0 to 16777215 )
+   * @returns {number}
+   */
+  public readUInt24LE(): number {
+    const value = this.readByte() |
+    this.readByte() << 8 |
+    this.readByte() << 16
+
+    if (value < 0 || value > 16777215) return null
+
+    return value
+  }
+
+  /**
+   * Writes a 24 bit ( 3 bytes ) unsigned little eddian integer ( 0 to 16777215 )
+   * @param value 
+   */
+  public writeUInt24LE(value: number): void {
+    if (value < 0 || value > 16777215) throw Error('Value must be between 0 and 16777215')
+    this.writeByte(value)
+    this.writeByte(value >> 8)
+    this.writeByte(value >> 16)
   }
 }
 
