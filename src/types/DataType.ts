d7b00aa9084a2de1c04eae0c11ee089eefdd783f
PMK744 <69542280+PMK744@users.noreply.github.com>
2023-10-30 22:06:36 -0500

    added extra value slot for data types

--- src/types/DataType.ts
+++ src/types/DataType.ts
@@ -2,10 +2,10 @@ import type { BinaryStream } from '../BinaryStream'
 import { Endianness } from '../Endianness'
 
 export abstract class DataType {
-  public static read(stream: BinaryStream, endian: Endianness = Endianness.Big): any {
+  public static read(stream: BinaryStream, endian: Endianness = Endianness.Big, value: any): any {
     throw new Error('Not implemented')
   }
-  public static write(stream: BinaryStream, value: any, endian: Endianness = Endianness.Big): void {
+  public static write(stream: BinaryStream, value: any, endian: Endianness = Endianness.Big, value2: any): void {
     throw new Error('Not implemented')
   }
 }
